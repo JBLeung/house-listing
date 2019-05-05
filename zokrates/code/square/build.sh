4af4b1870d0cf12b16771c75377367f65c3946db
Carlton Joseph <carlton.joseph@gmail.com>
2019-05-05 09:44:09 -0700

    add zokrate changes

--- zokrates/code/square/build.sh
+++ zokrates/code/square/build.sh
@@ -5,10 +5,12 @@ rm -rf proving.key
 rm -rf verification.key
 rm -rf verifier.sol
 rm -rf witness
+# below for v4 comment out for v3
+params="--proving-scheme pghr13"
 if test -f "../../zokrates"; then
 	../../zokrates compile -i square.code
-	../../zokrates setup --proving-scheme pghr13
+	../../zokrates setup $params
 	../../zokrates compute-witness -a 3 9
-	../../zokrates generate-proof --proving-scheme pghr13
-	../../zokrates export-verifier --proving-scheme pghr13
+	../../zokrates generate-proof $params
+	../../zokrates export-verifier $params
 fi
